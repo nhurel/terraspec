3c763f5c4f0a3e8ce732816604715a69c694e1c4
Nathan Hurel <nhurel@gmail.com>
2020-10-27 23:26:12 +0100

    Add test for GetFake method

--- lib/providers_test.go
+++ lib/providers_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/hashicorp/terraform/addrs"
 	"github.com/hashicorp/terraform/plugin/discovery"
+	"github.com/zclconf/go-cty/cty"
 )
 
 func TestBuildProviderResolver(t *testing.T) {
@@ -78,4 +79,100 @@ func TestBuildProviderResolverLegacy(t *testing.T) {
 	if pluginMeta != expectedMeta {
 		t.Errorf("PluginMeta not correct. Got %v. Expected %v.", pluginMeta, expectedMeta)
 	}
+}
+
+func TestGetFake(t *testing.T) {
+
+	commonQuery := cty.ObjectVal(map[string]cty.Value{
+		"key":      cty.StringVal("good-key"),
+		"property": cty.StringVal("good-value"),
+	})
+
+	goodResult := cty.ObjectVal(map[string]cty.Value{
+		"id": cty.NumberIntVal(1000),
+	})
+	badResult := cty.ObjectVal(map[string]cty.Value{
+		"name": cty.StringVal("bad result"),
+	})
+
+	tests := map[string]struct {
+		typeName string
+		query    cty.Value
+		expected *Assert
+		others   []*Assert
+	}{
+		"no fakes": {
+			typeName: "type",
+			query:    commonQuery,
+			expected: &Assert{Return: cty.NilVal},
+			others:   []*Assert{},
+		},
+		"no good typeName": {
+			typeName: "type",
+			query:    commonQuery,
+			expected: &Assert{TypeName: TypeName{Type: "other", Name: "bad"}, Return: cty.NilVal},
+			others:   []*Assert{},
+		},
+		"one good typeName": {
+			typeName: "type",
+			query:    commonQuery,
+			expected: &Assert{
+				TypeName: TypeName{Type: "type", Name: "good"},
+				Value: cty.ObjectVal(map[string]cty.Value{
+					"key":      cty.StringVal("bad-key"),
+					"property": cty.StringVal("bad-value"),
+				}),
+				Return: goodResult},
+			others: []*Assert{
+				{
+					TypeName: TypeName{Type: "other", Name: "wrong"},
+					Value:    commonQuery,
+					Return:   badResult,
+				},
+			},
+		},
+		"best good typeName": {
+			typeName: "type",
+			query:    commonQuery,
+			expected: &Assert{
+				TypeName: TypeName{Type: "type", Name: "good"},
+				Value: cty.ObjectVal(map[string]cty.Value{
+					"key":      cty.StringVal("good-key"),
+					"property": cty.StringVal("bad-value"),
+				}),
+				Return: goodResult},
+			others: []*Assert{
+				{
+					TypeName: TypeName{Type: "other", Name: "wrong"},
+					Value:    commonQuery,
+					Return:   badResult,
+				},
+				{
+					TypeName: TypeName{Type: "type", Name: "good"},
+					Value: cty.ObjectVal(map[string]cty.Value{
+						"key":      cty.StringVal("bad-key"),
+						"property": cty.StringVal("bad-value"),
+					}),
+					Return: badResult,
+				},
+			},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			frc := &FakeResourceCreator{}
+			frc.SetFakes(append(tt.others, tt.expected))
+			got := frc.GetFake(tt.typeName, tt.query)
+
+			if got.IsNull() && tt.expected.Return.IsNull() {
+				t.Logf("GetFake returned cty.NilVal as expected")
+			} else {
+				if (got.IsNull() != tt.expected.Return.IsNull()) || !got.RawEquals(tt.expected.Return) {
+					t.Errorf("GetFake didn't return expected value. Got: %v\n Expected: %v", got, tt.expected.Return)
+				}
+			}
+		})
+	}
+
 }
